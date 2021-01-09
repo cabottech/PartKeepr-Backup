1af98dae81dc81c8ed752c1bcc9c2a7673d5c1a9
Darian Cabot <darian@cabottechnologies.com>
2021-01-09 12:09:27 +0930

    Minor improvements to bash script and README

--- README.md
+++ README.md
@@ -13,28 +13,29 @@ This script is intended to work with [PartKeepr 1.4.0 on Raspbian 9.13 (stretch)
 
 Install prerequisites:
 
-```
-$ sudo apt-get update
-$ sudo apt-get install git zip
+```bash
+sudo apt-get update
+sudo apt-get install git zip
 ```
 
 Change to the home directory and clone from git:
 
-```
-$ cd ~
-$ git clone https://github.com/cabottech/PartKeepr-Backup.git
+```bash
+cd ~
+git clone https://github.com/cabottech/PartKeepr-Backup.git
 ```
 
 Change to the script directory and set permissions to execute the script:
 
-```
-$ cd PartKeepr-Backup
-$ chmod +x partkeepr-backup.sh
+```bash
+cd PartKeepr-Backup
+chmod +x partkeepr-backup.sh
 ```
 
 Set the database and file properties (see [Configuration](##Configuration) for details):
-```
-$ nano partkeepr-backup.properties
+
+```bash
+nano partkeepr-backup.properties
 ```
 
 ## Configuration
@@ -52,22 +53,23 @@ Note: it is recommended that `backup_root_path` be set to a location *not* on th
 ### From command-line
 
 Run a backup:
-```
+
+```bash
 cd ~/PartKeepr-Backup
-$ ./partkeepr-backup.sh
+./partkeepr-backup.sh
 ```
 
 ### From cron
 
 To automate the backup each day:
 
-```
-$ crontab -e
+```bash
+crontab -e
 ```
 
 Add the following line to schedule a backup at 2:15AM each day. Adjust to your install path if needed:
 
-```
+```bash
 15 2 * * * cd /home/pi/PartKeepr-Backup && /bin/sh ./partkeepr-backup.sh
 ```
 
@@ -76,32 +78,37 @@ Add the following line to schedule a backup at 2:15AM each day. Adjust to your i
 The following is a simple example of mounting a CIFS/SAMBA network share folder and configuring this as the backup storage location.
 
 First, create a mount location:
-```
-$ sudo mkdir /mnt/backups
+
+```bash
+sudo mkdir /mnt/backups
 ```
 
 Mount the network share folder:
 
 Options:
+
 - `//fileserver.local/backups/PartKeepr` is the path to an existing network share folder. An IP address can also be used, i.e. `//192.168.1.123/backups/PartKeepr`.
 - `username=remoteuser` is to authenticate with the file server (you will be prompted for a password if applicable)
 - `uid=pi` is to set local user `pi` as the owner of the share to allow read/write access.
 - `noexec` disables executing anything from the share location (an optional safety precaution).
-```
-$ sudo mount -t cifs //fileserver.local/backups/PartKeepr /mnt/backups -o username=remoteuser,uid=pi,noexec
+
+```bash
+sudo mount -t cifs //fileserver.local/backups/PartKeepr /mnt/backups -o username=remoteuser,uid=pi,noexec
 ```
 
 Enter your network share password when prompted.
 
 Now that we have the network share connected, `partkeepr-backup.preferences` can be set accordingly. Update the backup location:
-```
+
+```bash
 backup_root_path=/mnt/backups
 ```
 
 Run a backup to check this is all working:
-```
-$ cd ~/PartKeepr-Backup
-$ ./partkeepr-backup.sh
+
+```bash
+cd ~/PartKeepr-Backup
+./partkeepr-backup.sh
 ```
 
 The backup files should be now stored the network share folder.
@@ -151,8 +158,8 @@ This log details the date of the backup, files written, backup size, and duratio
 
 Example log output:
 
-```
-PartKeepr Backup 0.1.2
+```text
+PartKeepr Backup 0.1.3
 
 Database backup:
 Retrieving database SQL...
@@ -160,23 +167,23 @@ Retrieving database SQL...
 Compressing backup to ZIP archive...
 * Success
 Database backup summary:
-* File name:   20200907-021501_partkeepr-database-backup.sql.zip
-* File size:   55K
+* File name:   20210109-123514_partkeepr-database-backup.sql.zip
+* File size:   70K
 * Duration:    0:00:01
 
 Web data backup:
 Compressing web data to ZIP archive...
 * Success
 Web data backup summary:
-* File name:   20200907-021501_partkeepr-data-backup.zip
-* File size:   15M
-* Duration:    0:00:08
+* File name:   20210109-123514_partkeepr-data-backup.zip
+* File size:   21M
+* Duration:    0:00:09
 
 Web config backup:
 Compressing web config to ZIP archive...
 * Success
 Web config backup summary:
-* File name:   20200907-021501_partkeepr-config-backup.zip
+* File name:   20210109-123514_partkeepr-config-backup.zip
 * File size:   20K
 * Duration:    0:00:00
 ```
--- partkeepr-backup.sh
+++ partkeepr-backup.sh
@@ -16,7 +16,7 @@
 # - Licence: MIT (see the LICENSE file)
 
 
-ver="0.1.2"
+ver="0.1.3"
 echo "PartKeepr Backup $ver"
 
 # Source the script settings
@@ -27,29 +27,29 @@ backup_database() {
 	local start=$(date +%s)
 	local backup_file="${date_start}_partkeepr-database-backup.sql"
 
-	echo "Database backup:" | tee -a $backup_path/$log_file
-	echo "Retrieving database SQL..." | tee -a $backup_path/$log_file
-	res="$( ( mysqldump --opt --host=$database_host --user=$database_user --password=$database_pass $database_name > $backup_path/$backup_file ) 2>&1 )"
+	echo "Database backup:" | tee -a "$backup_path/$log_file"
+	echo "Retrieving database SQL..." | tee -a "$backup_path/$log_file"
+	res="$( ( mysqldump --opt --host=$database_host --user=$database_user --password=$database_pass $database_name > "$backup_path/$backup_file" ) 2>&1 )"
 	if $res
 	then
-		echo "* Success" | tee -a $backup_path/$log_file
+		echo "* Success" | tee -a "$backup_path/$log_file"
 	else
-		echo "* ERROR: $res" | tee -a $backup_path/$log_file
+		echo "* ERROR: $res" | tee -a "$backup_path/$log_file"
 		return 1
 	fi
 
-	echo "Compressing backup to ZIP archive..." | tee -a $backup_path/$log_file
+	echo "Compressing backup to ZIP archive..." | tee -a "$backup_path/$log_file"
 	# Zip with maximum compression. Run at low priority.
 	res="$( nice -n 10 zip -j -m -q -T -9 "$backup_path/$backup_file.zip" "$backup_path/$backup_file" )"
 	if $res
 	then
-		echo "* Success" | tee -a $backup_path/$log_file
+		echo "* Success" | tee -a "$backup_path/$log_file"
 	else
-		echo "* ERROR: $res" | tee -a $backup_path/$log_file
+		echo "* ERROR: $res" | tee -a "$backup_path/$log_file"
 		return 1
 	fi
 
-	echo "Database backup summary:" | tee -a $backup_path/$log_file
+	echo "Database backup summary:" | tee -a "$backup_path/$log_file"
 	backup_summary "$backup_file.zip" "$start"
 }
 
@@ -58,19 +58,19 @@ backup_app_data() {
 	local start=$(date +%s)
 	local backup_file="${date_start}_partkeepr-data-backup.zip"
 
-	echo "Web data backup:" | tee -a $backup_path/$log_file
-	echo "Compressing web data to ZIP archive..." | tee -a $backup_path/$log_file
+	echo "Web data backup:" | tee -a "$backup_path/$log_file"
+	echo "Compressing web data to ZIP archive..." | tee -a "$backup_path/$log_file"
 	# Zip with maximum compression. Run at low priority.
 	res="$( nice -n 10 zip -q -r -T -9 "$backup_path/$backup_file" "$partkeepr_data_path" )"
 	if $res
 	then
-		echo "* Success" | tee -a $backup_path/$log_file
+		echo "* Success" | tee -a "$backup_path/$log_file"
 	else
-		echo "* ERROR: $res" | tee -a $backup_path/$log_file
+		echo "* ERROR: $res" | tee -a "$backup_path/$log_file"
 		return 1
 	fi
 
-	echo "Web data backup summary:" | tee -a $backup_path/$log_file
+	echo "Web data backup summary:" | tee -a "$backup_path/$log_file"
 	backup_summary "$backup_file" "$start"
 }
 
@@ -79,19 +79,19 @@ backup_app_config() {
 	local start=$(date +%s)
 	local backup_file="${date_start}_partkeepr-config-backup.zip"
 
-	echo "Web config backup:" | tee -a $backup_path/$log_file
-	echo "Compressing web config to ZIP archive..." | tee -a $backup_path/$log_file
+	echo "Web config backup:" | tee -a "$backup_path/$log_file"
+	echo "Compressing web config to ZIP archive..." | tee -a "$backup_path/$log_file"
 	# Zip with maximum compression. Run at low priority.
 	res="$( nice -n 10 zip -q -r -T -9 "$backup_path/$backup_file" "$partkeepr_config_path" )"
 	if $res
 	then
-		echo "* Success" | tee -a $backup_path/$log_file
+		echo "* Success" | tee -a "$backup_path/$log_file"
 	else
-		echo "* ERROR: $res" | tee -a $backup_path/$log_file
+		echo "* ERROR: $res" | tee -a "$backup_path/$log_file"
 		return 1
 	fi
 
-	echo "Web config backup summary:" | tee -a $backup_path/$log_file
+	echo "Web config backup summary:" | tee -a "$backup_path/$log_file"
 	backup_summary "$backup_file" "$start"
 }
 
@@ -101,17 +101,17 @@ backup_app_config() {
 # $2 is the start time - i.e. '$(date +%s)'
 # Requires the global variable $backup_path be set.
 backup_summary() {
-	echo "* File name:   $1" | tee -a $backup_path/$log_file
+	echo "* File name:   $1" | tee -a "$backup_path/$log_file"
 	local filesize=$(ls -lsah "$backup_path/$1" | awk '{print $6}')
-	echo "* File size:   $filesize" | tee -a $backup_path/$log_file
+	echo "* File size:   $filesize" | tee -a "$backup_path/$log_file"
 
 	local dur_sec=$(( $(date +%s) - $2 ))
-	local hr=$(( $dur_sec / 3600 )) # Calculate hours.
-	local min=$(( ($dur_sec % 3600) / 60 )) # Calculate remaining minutes.
-	local sec=$(( $dur_sec % 60 )) # Calculate remaining seconds.
+	local hr=$(( dur_sec / 3600 )) # Calculate hours.
+	local min=$(( (dur_sec % 3600) / 60 )) # Calculate remaining minutes.
+	local sec=$(( dur_sec % 60 )) # Calculate remaining seconds.
 	min=$(printf "%02d" $min) # Ensure two digits (zero padding).
 	sec=$(printf "%02d" $sec) # Ensure two digits (zero padding).
-	echo "* Duration:    $hr:$min:$sec\n" | tee -a $backup_path/$log_file
+	echo "* Duration:    $hr:$min:$sec\n" | tee -a "$backup_path/$log_file"
 }
 
 
@@ -120,12 +120,12 @@ date_start=$(date +'%Y%m%d-%H%M%S')
 backup_path="$backup_root_path/$(date +%Y%m)"
 log_file="${date_start}_partkeepr-backup.log"
 
-echo "PartKeepr Backup $ver\n" > $backup_path/$log_file
+echo "PartKeepr Backup $ver\n" > "$backup_path/$log_file"
 echo "* Backup path: $backup_path"
 echo "* Log: $log_file\n"
 
 # Create backup path...
-mkdir -p $backup_path
+mkdir -p "$backup_path"
 
 # Run backups
 backup_database
